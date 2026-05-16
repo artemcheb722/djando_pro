781ef16ad8719b949494356a5903f31d224263fc
Artem <artem.chebanyuk@gmail.com@email.com>
2026-05-16 21:43:24 +0300

    dd

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z+l6j!=%&xvhd$j8t&hnhj1rs2ck=-(zv^to@m%pltzg885)^!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
