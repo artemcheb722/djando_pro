f82b7ac5513677da50bc9d2bd1f7f86335d6c1a2
Artem <artem.chebanyuk@gmail.com@email.com>
2026-07-24 20:07:41 +0300

    added nginx reverse proxy, move DJANGO_SECRET_KEY to .env

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ except ImportError:
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z+l6j!=%&xvhd$j8t&hnhj1rs2ck=-(zv^to@m%pltzg885)^!'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
