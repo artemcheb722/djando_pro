86c0935a50ee00726d5d44b1b05a4243bb5f1b4d
Artem <artem.chebanyuk@gmail.com@email.com>
2026-08-10 21:23:09 +0300

    Setup project b, а separate database has been added, and a table has been added

---
+++ project_b/apps/analytics/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class UserPurchase(models.Model):
+    user_id = models.IntegerField()
+    order_id = models.IntegerField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
